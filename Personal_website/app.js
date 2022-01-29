5a4ebe9e8f663d2034c265ac48413d37344402ee
Reid Shipley <r.w.shipley7@gmail.com>
2022-01-28 19:04:06 -0600

    Feeling happy with my home page for now, may come back to add modal, spotify link, etc.

--- Personal_website/app.js
+++ Personal_website/app.js
@@ -1,6 +1,21 @@
 $(() => {
-console.log(`jquery active`)
+$(`.carousel-image-home`).eq(0).css(`display`, `block`)
 
+let imageNum = 0;
+let imageIndex = $(`#picture-box`).children().length-1;
 
-
+$(`.next`).on(`click`, () => {
+  $(`.carousel-image-home`).eq(imageNum).fadeOut(500)
+  $(`.carousel-image-home`).delay(500)
+  if (imageNum >= imageIndex)
+    {
+    imageNum = 0;
+    }
+  else
+    {
+    imageNum++;
+    }
+$(`.carousel-image-home`).eq(imageNum).fadeIn(500)
 });
+
+})
